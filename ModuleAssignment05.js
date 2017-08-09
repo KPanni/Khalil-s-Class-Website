5e619b9dadfff4e339b27fed9e32ab6a0c28d5d0
KPanni <k.itagiba@gmail.com>
2017-08-08 23:09:33 -0700

    Update ModuleAssignment05.js

--- ModuleAssignment05.js
+++ ModuleAssignment05.js
@@ -1,4 +1,4 @@
 var person = prompt("What is your name?", "");
 
 if (person != null) {
-    document.getElementById("demo").innerHTML = "Welcome " + person + "!";} 
+    document.getElementById("demo").innerHTML ="Welcome " + person + "!";} 
